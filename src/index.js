1bff69fd1337dc6a50826ddd06fa1f633040f488
Gustavo Justino <justinofgustavo@gmail.com>
2023-04-20 21:59:19 -0300

    Setting steps to chat

--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
 const { Client, LocalAuth } = require("whatsapp-web.js");
 const qrCode = require("qrcode-terminal");
 
-const client = new Client({								// instace Client and set auth strategy
-	authStrategy: new LocalAuth()
+// =============================================================================
+
+const storage = Object.create({});
+
+function getStage({ from }) {
+	if (!storage[from]) {
+		storage[from] = {
+			stage: 0,
+			tree: 0,
+		};
+	}
+
+	return storage[from].stage;
+}
+
+const stages = [
+	{
+		label: "Start",
+		exec({ from }) {
+			storage[from].stage = 1;
+			return "Olá, escolha uma opção:\n1 - Op 1\n2 - Op 2\n3 - Op 3";
+		},
+	},
+];
+
+// =============================================================================
+
+const client = new Client({
+	// instace Client and set auth strategy
+	authStrategy: new LocalAuth(),
 });
 
-
-client.on("qr", (code) => {								// on client generate QR Code display in terminal
+client.on("qr", (code) => {
+	// on client generate QR Code display in terminal
 	qrCode.generate(code, { small: true });
 });
 client.on("ready", () => {
 	console.log("Whatsapp is ready.");
 });
 
+client.on("message", (message) => {
+	const currentStage = getStage({ from: message.from });
+
+	const messageResponse = stages[currentStage].exec({
+		from: message.from,
+	});
+
+	if (messageResponse) {
+		client.sendMessage(message.from, messageResponse);
+	}
+});
 
 client.initialize();
